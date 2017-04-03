9a4f6a362eff89897d0837fcc0af918e832ae589
Dimas J. Taniawan <dimas.taniawan@bocistudio.com>
2017-04-03 16:35:17 +0700

    prepare boilerplate for rest api

--- server/dev.js
+++ server/dev.js
@@ -1,13 +1,9 @@
 import express from 'express'
 import morgan from 'morgan'
 import responseTime from 'response-time'
-import webpackDevMiddleware from 'webpack-dev-middleware'
-import webpack from 'webpack'
 
 import fs from 'fs'
 import path from 'path'
-
-import webpackConfig from './webpack/dev'
 
 const logFile = path.resolve(__dirname, '../log/dev.log')
 const logDir = path.resolve(__dirname, '../log')
@@ -17,22 +13,9 @@ const PORT = process.env.PORT || 3000
 fs.existsSync(logDir) || fs.mkdirSync(logDir)
 
 const app = express()
-const compiler = webpack(webpackConfig)
-const middleware = webpackDevMiddleware(compiler, {
-  publicPath: '/assets',
-  stats: {
-    color: true,
-  },
-})
 
 app.use(morgan('combined', { stream: accessLogStream }))
 app.use(responseTime())
-app.use(middleware)
-
-app.get('*', (req, res) => {
-  res.write(middleware.fileSystem.readFileSync(path.resolve(__dirname, 'assets/index.html')))
-  res.end()
-})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
